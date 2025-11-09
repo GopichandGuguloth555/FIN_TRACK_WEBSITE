90c9be8656e13055ff32d06bad1493e28fe949b3
Gopichand Guguloth <gopichandguguloth.123@gmail.com>
2025-11-09 21:40:44 +0530

    Initialised Frontend To The Project with React and Tailwindcss!

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-900 text-red-500 ">
+      <h1 className="text-6xl font-bold">Gopichand Guguloth!</h1>
+    </div>
+  )
+}
+
+export default App
