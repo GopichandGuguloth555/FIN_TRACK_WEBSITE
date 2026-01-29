c33b10945ad9315c5e5ba7cf1267c2b5edb8ae6d
Gopichand Guguloth <gopichandguguloth.123@gmail.com>
2026-01-29 22:22:24 +0530

    feat:login page ui

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import Component from "@/components/ui/login-1";
+
+const Login= () => {
+  return (
+    <div className=" w-screen h-screen justify-center items-center">
+      <Component />
+    </div>
+  );
+};
+
+export { Login };
