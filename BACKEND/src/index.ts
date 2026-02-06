e909b8cfb6975448d1ef358a0e97b2d3daf62216
Gopichand Guguloth <gopichandguguloth.123@gmail.com>
2026-02-07 00:23:07 +0530

    fix: axios withCredentials enabled

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -16,11 +16,13 @@ const PORT = Number(process.env.PORT) || 5000;
 const app = express();
 
 
-const allowedOrigins = (
-  process.env.FRONTEND_URL || "http://localhost:5173"
-)
-  .split(",")
+const allowedOrigins = process.env.FRONTEND_URL
+  ?.split(",")
   .map((url) => url.trim());
+
+if (!allowedOrigins || allowedOrigins.length === 0) {
+  throw new Error("❌ FRONTEND_URL is not defined in environment variables");
+}
 
 app.use(
   cors({
@@ -38,6 +40,7 @@ app.use("/uploads", express.static("uploads"));
 
 connectDB();
 app.use(passport.initialize());
+
 
 app.use("/user", userRoutes);
 app.use("/imports", importsRoutes);
--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,6 +3,7 @@ import { API_BASE_URL } from "@/utils/constants";
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true, 
 });
 
 instance.interceptors.request.use((config) => {
